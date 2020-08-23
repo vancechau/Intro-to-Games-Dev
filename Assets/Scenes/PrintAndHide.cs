5276e6401bd2667a16388aceb0ca429a0cf59ebc
vancechau <50088207+vancechau@users.noreply.github.com>
2020-08-23 22:47:49 +1000

    100 percent complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
 {
-
     private int i;
-
+    private int blueNone;
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 0;
+        blueNone = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(this.gameObject.name + ": " + i);
+
+        if (i == 200 && this.gameObject.tag == "Red")
+        {
+            rend.enabled = false;
+        }
+
+        if (i == blueNone && this.gameObject.tag == "Blue")
+        {
+            rend.enabled = false;
+        }
+
         i++;
     }
 }

67fbdba4f15a207f23889d04c1e2a3c4315f083f
vancechau <50088207+vancechau@users.noreply.github.com>
2020-08-23 22:30:33 +1000

    80 percent complete

---
+++ Assets/Scenes/ConsolePrint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    private int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.gameObject.name + ": " + i);
+        i++;
+    }
+}

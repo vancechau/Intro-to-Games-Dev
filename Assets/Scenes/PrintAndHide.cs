b734e5e3d241b6171837cba4b20fb8876ec8478e
vancechau <50088207+vancechau@users.noreply.github.com>
2020-08-23 22:37:37 +1000

    90 percent complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
 
     private int i;
+
+    public Renderer rend;
 
     // Start is called before the first frame update
     void Start()

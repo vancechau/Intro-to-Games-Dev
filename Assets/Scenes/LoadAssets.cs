72d59656093d5adbe636852cbded9ffe5f69d0df
vancechau <50088207+vancechau@users.noreply.github.com>
2020-08-23 22:23:52 +1000

    70 percent complete

--- Assets/Scenes/LoadAssets.cs
+++ Assets/Scenes/LoadAssets.cs
@@ -7,10 +7,14 @@ public class LoadAssets : MonoBehaviour
     
     public GameObject redObj;
     
+    [SerializeField]
+    private GameObject blueObj;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
